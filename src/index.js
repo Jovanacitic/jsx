8b7fd7fa190a62f2de84e613cbfb4e6d406efcf0
Jovana <citicjovana@gmail.com>
2020-05-03 19:22:17 +0200

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+const App = () => {
+    return <div> Hello Jovana!</div>;
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
